af71f4d9847805591af6e91f6c77392b6904dfee
alexyzer <132060385+alexyzer@users.noreply.github.com>
2026-06-08 23:28:48 +0700

    Was there a good reason to? Crashes with connector because Lapisworks loads long after HexCasting gets remapped by Forge. Loads and works just as well on fabric.

--- src/main/java/com/luxof/lapisworks/mixin/GuiSpellcastingMixin.java
+++ src/main/java/com/luxof/lapisworks/mixin/GuiSpellcastingMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = GuiSpellcasting.class, remap = false)
+@Mixin(value = GuiSpellcasting.class)
 public class GuiSpellcastingMixin {
     @Inject(
         method = "tick",
