7bd1331a18a06e83309455a927665336fde20e10
alexyzer <132060385+alexyzer@users.noreply.github.com>
2026-06-08 21:26:28 +0700

    Get the Item from registry to bypass Oneironaut Forge differences.

--- src/main/java/com/luxof/lapisworks/interop/oneironaut/FuckingInexhaustiblePhials.java
+++ src/main/java/com/luxof/lapisworks/interop/oneironaut/FuckingInexhaustiblePhials.java
@@ -12,10 +12,16 @@ import net.beholderface.oneironaut.item.BottomlessMediaItem;
 import net.beholderface.oneironaut.registry.OneironautItemRegistry;
 
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
 
 public class FuckingInexhaustiblePhials {
+    //Get the item from registry to prevent crashing with Oneironaut Forge
+    static final Item BOTTOMLESS_MEDIA_ITEM = Registries.ITEM.get(new Identifier("oneironaut", "endless_phial"));
+
     public static long getBottomlessContrib(PlayerBasedCastEnv ctx) {
         ServerPlayerEntity player = ((ServerPlayerEntity)ctx.getCastingEntity());
 
@@ -28,7 +34,6 @@ public class FuckingInexhaustiblePhials {
                 return trinket.isOf(OneironautItemRegistry.BOTTOMLESS_CASTING_ITEM.get())
                     ? IXplatAbstractions.INSTANCE.findMediaHolder(trinket).getMedia() : 0L;
         }
-
         PlayerInventory inv = player.getInventory();
         List<ItemStack> items = new ArrayList<>();
         items.addAll(inv.main);
@@ -36,9 +41,7 @@ public class FuckingInexhaustiblePhials {
         items.addAll(inv.armor);
         long total = 0L;
         for (ItemStack item : items) {
-            total += item.isOf(
-                OneironautItemRegistry.BOTTOMLESS_MEDIA_ITEM.get()
-            ) ? ((BottomlessMediaItem)item.getItem()).getMedia(item) : 0;
+            total += item.isOf(BOTTOMLESS_MEDIA_ITEM) ? ((BottomlessMediaItem)item.getItem()).getMedia(item) : 0;
         }
         return total;
     }
